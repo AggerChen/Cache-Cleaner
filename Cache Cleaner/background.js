77126658e79088ba46cae4aaff887ed885ed1e9b
chenhx <chenhx@cdfortis.com>
2019-07-26 18:03:41 +0800

    chrome浏览器清理缓存插件

---
+++ Cache Cleaner/background.js
@@ -0,0 +1,46 @@
+﻿
+//监听页面
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    
+    if(request.msg == 'cleanCache'){		//如果是清理命令
+		var days = request.days||1;
+		toclean(days,request.data);			//则调用执行清除方法
+    }
+	sendResponse({farewell:true});			//返回信息
+});
+
+//监听快捷键
+chrome.commands.onCommand.addListener(function(command) {
+
+  if (command == "cleanKey") {				//如果快捷键是指定的按钮
+    toclean(1,{ "appcache": true,			//则调用执行清除方法
+                "cache": true,
+                "cookies": false,
+                "downloads": false,
+                "fileSystems": true,
+                "formData": true,
+                "history": false,
+                "indexedDB": false,
+                "localStorage": true,
+                "serverBoundCertificates": false,
+                "pluginData": false,
+                "passwords": false,
+                "webSQL": false});
+  }
+});
+
+//执行清除方法
+function toclean(days,data){
+	
+	var millisecondsPerWeek = 1000 * 60 * 60 * 24 * days;
+	var ago = (new Date()).getTime() - millisecondsPerWeek;
+	alert(JSON.stringify(data));
+	chrome.browsingData.remove({ "since": ago }, data , function () {
+		//弹出框
+		new Notification('Cache Cleaner', {
+			icon: 'clean48.png',
+			body: '清理缓存成功! successfully! '	
+		});
+	});
+	
+}
---
+++ Cache Cleaner/popup.js
@@ -0,0 +1,71 @@
+﻿/*
+* author:aggerChen
+*/
+
+let data1 = {
+	appcache: false,
+	cache: false,
+	cookies: false,
+	downloads: false,
+	fileSystems: false,
+	formData: false,
+	history: false,
+	indexedDB: false,
+	localStorage: false,
+	passwords: false,
+	pluginData: false,
+	serverBoundCertificates: false,
+	webSQL: false
+}
+
+layui.use(['form'], function(){
+  var form = layui.form,layer = layui.layer,laytpl = layui.laytpl;
+  
+	 
+	
+	form.on('submit(cleanFilter)', function(e){
+		let data2 = e.field;
+		let time = e.time;
+		delete data2.time;
+		if(Object.keys(data2).length<=0){
+			layer.msg('请选择 (please choose)');
+			return false;
+		}
+		let data = Object.assign({},data1,data2);
+		let json = formatterData(data);
+		console.log(json);
+		toClean(time,json);
+	}); 
+});
+
+//去清理
+function toClean(time,json){
+		
+	$("#cleanBtn").attr('disabled',true);
+	$("#showMsg").text('缓存清理中...');
+	
+	//向扩展程序发送消息，并传递数据
+   chrome.runtime.sendMessage({
+		msg: 'cleanCache',
+		data: json,			//获取清除选项			
+		days: time			//获取清除多长时间
+	},function(response){
+	   //响应函数
+		$("#cleanBtn").attr('disabled',false);
+		$("#showMsg").text('清理缓存成功! successfully!');
+		setTimeout(()=>{
+			$("#showMsg").text('快捷键(hot key)：Alt+C');
+		},1000);
+   });
+       
+}
+
+//获取选中的清理选项
+function formatterData(obj){
+	
+	Object.keys(obj).forEach(function(key){
+		obj[key] = Boolean(obj[key]);
+	});
+	
+	return obj;
+}
